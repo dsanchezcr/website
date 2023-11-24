0894642345d4d8cd5d428cbc648b4264c9151c3e
David Sanchez | Principal DevOps Consultant <dsanchezcr@outlook.com>
2023-11-24 15:29:03 -0600

    fixing issue with module

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@dsanchezcr/colonesexchangerate': './node_modules/@dsanchezcr/colonesexchangerate',
+        },
+      },
+    ],
+  ],
 };
--- types.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@dsanchezcr/colonesexchangerate';
